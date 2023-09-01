f93facc99e1d169c1b58465adfd209b6ef14e4b0
ramsha1353 <136148296+ramsha1353@users.noreply.github.com>
2023-09-01 11:42:54 +0500

    Add files via upload
    final

---
+++ ramshaProserve/proserve_app/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from proserve_app.models import UserProfile, WorkerProfile
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False  # Prevents users from deleting the user profile when editing the User
+
+# Register the inline admin class
+UserAdmin.inlines = (UserProfileInline,)
+
+# Re-register the UserAdmin class
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+
+# Unregister the default UserAdmin and register your custom UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+# Register your models here.
+# @admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user',  'phone_no','Address','user_type']
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+
+
+
+@admin.register(WorkerProfile)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = [ 'profile', 'service', 'description', 'price_per_day','experiance', 'available_time' ]
+
+
---
+++ ramshaProserve/proserve_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Address = models.CharField(max_length=800)
+    phone_no = models.CharField(max_length=20)
+    user_type = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.user.username
+
+
+
+
+class WorkerProfile(models.Model):
+    profile= models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    service = models.CharField(max_length=100)
+    description = models.TextField()
+    experiance = models.CharField(max_length=100, blank=True)
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    available_time = models.CharField(max_length=100)
+   
+    
